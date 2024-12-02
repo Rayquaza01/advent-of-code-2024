7373b6f89a7f133cfac31eab84a346de37bbe4e2
Arnaught <rayquaza01@outlook.com>
2024-12-01 21:23:26 -0500

    Use local time for date

--- scripts/start.lua
+++ scripts/start.lua
@@ -5,4 +5,4 @@ store("/ram/system/pwc.pod", "/projects/advent-of-code-2024/aoc2024.p64")
 send_message(3, { event = "clear_project_workspaces" })
 send_message(3, { event = "run_pwc", argv = env().argv, path = env().path })
 
-notify("Running Advent of Code at " .. date())
+notify("Running Advent of Code at " .. date("%Y-%m-%d %H:%M:%S"))
