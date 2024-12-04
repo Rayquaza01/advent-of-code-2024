2bf3a6b19a21a0787b7e012f687cdd5e21ab6d9c
Arnaught <rayquaza01@outlook.com>
2024-12-04 13:04:26 -0500

    Refactor part 2

--- src/day04/main2.lua
+++ src/day04/main2.lua
@@ -22,43 +22,51 @@ local autoscroll
 
 local cor
 
---- @param x integer
---- @param y integer
-local function search_grid(x, y)
-	-- pph.info(string.format("Testing tile %d, %d", x, y))
+local function search_grid()
+	for y = 1, height, 1 do
+		for x = 1, width, 1 do
+			-- pph.info(string.format("Testing tile %d, %d", x, y))
 
-	if grid[y][x] == "A" then
-		-- pph.info("Found an A")
+			position = { x = x, y = y }
 
-		adjacents = {
-			table.pack(g.find_adjacent(x, y, g.Directions.UP_LEFT)),
-			table.pack(g.find_adjacent(x, y, g.Directions.DOWN_RIGHT)),
-			table.pack(g.find_adjacent(x, y, g.Directions.UP_RIGHT)),
-			table.pack(g.find_adjacent(x, y, g.Directions.DOWN_LEFT))
-		}
+			if grid[y][x][1] == "A" then
+				-- pph.info("Found an A")
 
-		if every(adjacents, function (item) return g.is_in_bounds(item[1], item[2], width, height) end) then
-			local ul = grid[adjacents[1][2]][adjacents[1][1]]
-			local dr = grid[adjacents[2][2]][adjacents[2][1]]
-			local ur = grid[adjacents[3][2]][adjacents[3][1]]
-			local dl = grid[adjacents[4][2]][adjacents[4][1]]
+				adjacents = {
+					table.pack(g.find_adjacent(x, y, g.Directions.UP_LEFT)),
+					table.pack(g.find_adjacent(x, y, g.Directions.DOWN_RIGHT)),
+					table.pack(g.find_adjacent(x, y, g.Directions.UP_RIGHT)),
+					table.pack(g.find_adjacent(x, y, g.Directions.DOWN_LEFT))
+				}
 
-			if ((ul == "S" and dr == "M") or (ul == "M" and dr == "S")) and
-				((ur == "S" and dl == "M") or (ur == "M" and dl == "S")) then
-				for item in all(adjacents) do
-					add(matches, { x = item[1], y = item[2] })
-					add(matches, { x = x, y = y })
+				if every(adjacents, function (item) return g.is_in_bounds(item[1], item[2], width, height) end) then
+					local ul = grid[adjacents[1][2]][adjacents[1][1]][1]
+					local dr = grid[adjacents[2][2]][adjacents[2][1]][1]
+					local ur = grid[adjacents[3][2]][adjacents[3][1]][1]
+					local dl = grid[adjacents[4][2]][adjacents[4][1]][1]
+
+					if ((ul == "S" and dr == "M") or (ul == "M" and dr == "S")) and
+						((ur == "S" and dl == "M") or (ur == "M" and dl == "S")) then
+						for item in all(adjacents) do
+							grid[item[2]][item[1]][2] = 11
+							grid[y][x][2] = 11
+						end
+						total += 1
+
+						-- pph.info("Found an X-MAS")
+						-- yield()
+					else
+						-- pph.warn(ul .. "A" .. dr)
+						-- pph.warn(ur .. "A" .. dl)
+						-- pph.warn("Not an X-MAS")
+						-- yield()
+					end
+				else
+					-- pph.warn("OOB")
+					-- yield()
 				end
-				total += 1
-
-				-- pph.info("Found an X-MAS")
-			else
-				-- pph.warn(ul .. "A" .. dr)
-				-- pph.warn(ur .. "A" .. dl)
-				-- pph.warn("Not an X-MAS")
 			end
-		else
-			-- pph.warn("OOB")
+			yield()
 		end
 	end
 end
@@ -75,27 +83,21 @@ local function _init()
 	grid, width, height = load_input("day04/p1data.txt")
 
 	total = 0
-	matches = {}
 
-	position = 0
+	position = { x = 1, y = 1 }
 
 	cor = nil
 end
 
 local function _update()
-	local x, y = g.decode_pos(position, width, height)
+	local x, y = position.x, position.y
 
 	if cor == nil then
 		cor = cocreate(search_grid)
 	end
 
 	if costatus(cor) ~= "dead" then
-		coresume(cor, x, y)
-	end
-
-	if costatus(cor) == "dead" and position < width * height then
-		cor = nil
-		position += 1
+		coresume(cor)
 	end
 
 	if autoscroll then
@@ -134,21 +136,15 @@ end
 local function _draw()
 	cls()
 
-	local x, y = g.decode_pos(position, width, height)
+	local x, y = position.x, position.y
 
-	for i = 1, min(16, #grid), 1 do
-		for j = 1, min(32, #grid[i]), 1 do
-			print(grid[i + offsety][j + offsetx], (j - 1) * 8, (i - 1) * 8, 7)
+	for i = 1, min(16, height), 1 do
+		for j = 1, min(32, width), 1 do
+			print(grid[i + offsety][j + offsetx][1], (j - 1) * 8, (i - 1) * 8, grid[i + offsety][j + offsetx][2])
 		end
 	end
 
-	for m in all(matches) do
-		if g.is_in_bounds((m.x - offsetx) * 8, (m.y - offsety) * 8, 256, 128) then
-			print(grid[m.y][m.x], (m.x - offsetx - 1) * 8, (m.y - offsety - 1) * 8, 11)
-		end
-	end
-
-	print(grid[y][x], (x - offsetx - 1) * 8, (y - offsety - 1) * 8, 10)
+	print(grid[y][x][1], (x - offsetx - 1) * 8, (y - offsety - 1) * 8, 10)
 
 	rectfill(0, 120, 64, 128, 0)
 	print(string.format("\f7Total: \fe%d\f7", total), 0, 120)
